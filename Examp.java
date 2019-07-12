fc24be6f1ca3a757ae352d5f3c3825d5fd7f6ce4
Nishant <nishantsaini97286@gmail.com>
2019-07-12 17:51:14 +0530

    swing

---
+++ Examp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import javax.swing.*;
+public class Examp
+{
+public static void main(String args[])
+{
+JFrame jf=new JFrame("swing");
+jf.setSize(300,300);
+jf.setVisible(true);
+jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+}
